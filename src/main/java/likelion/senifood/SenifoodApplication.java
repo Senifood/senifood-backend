9543079ecb6a2336725aeb23a19688b175c00391
oseoko <oseoko@naver.com>
2024-07-31 22:22:28 +0900

    build: 초기 프로젝트 설정

---
+++ src/main/java/likelion/senifood/SenifoodApplication.java
@@ -0,0 +1,13 @@
+package likelion.senifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenifoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenifoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/senifood/SenifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.senifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
