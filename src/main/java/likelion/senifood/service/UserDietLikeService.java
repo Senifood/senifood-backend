30de844f2da14fb3f2ad6925f263d04488b48557
oseoko <oseoko@naver.com>
2024-08-03 10:39:23 +0900

    feat: UserDietLikeService에 필요한 함수 추가

--- src/main/java/likelion/senifood/service/UserDietLikeService.java
+++ src/main/java/likelion/senifood/service/UserDietLikeService.java
@@ -1,17 +1,23 @@
 package likelion.senifood.service;
 
+import likelion.senifood.entity.Diet;
 import likelion.senifood.entity.UserDietLike;
 import likelion.senifood.entity.UserDietLikeId;
 import likelion.senifood.repository.UserDietLikeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserDietLikeService {
 
     private final UserDietLikeRepository userDietLikeRepository;
+    private final DietService dietService;
 
-    public UserDietLikeService(UserDietLikeRepository userDietLikeRepository) {
+    public UserDietLikeService(UserDietLikeRepository userDietLikeRepository, DietService dietService) {
         this.userDietLikeRepository = userDietLikeRepository;
+        this.dietService = dietService;
     }
 
     public void saveUserDietLike(UserDietLike userDietLike) {
@@ -23,5 +29,13 @@ public class UserDietLikeService {
         id.setUserId(userId);
         id.setDietId(dietId);
         userDietLikeRepository.deleteById(id);
+    }
+
+    public List<Diet> getUserLikedDiets(String userId) {
+        List<UserDietLike> userLikes = userDietLikeRepository.findByUserId(userId);
+        return userLikes.stream()
+                .map(like -> dietService.findById(like.getDietId()).orElse(null))
+                .filter(diet -> diet != null)
+                .collect(Collectors.toList());
     }
 }
