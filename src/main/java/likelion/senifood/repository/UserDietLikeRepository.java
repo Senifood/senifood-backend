be8cfbc982b05f79461512497fa25696b203b5ce
oseoko <oseoko@naver.com>
2024-08-03 10:39:49 +0900

    feat: UserDietLikeService에 findByUserId 추가

--- src/main/java/likelion/senifood/repository/UserDietLikeRepository.java
+++ src/main/java/likelion/senifood/repository/UserDietLikeRepository.java
@@ -4,5 +4,8 @@ import likelion.senifood.entity.UserDietLike;
 import likelion.senifood.entity.UserDietLikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserDietLikeRepository extends JpaRepository<UserDietLike, UserDietLikeId> {
+    List<UserDietLike> findByUserId(String userId);
 }
