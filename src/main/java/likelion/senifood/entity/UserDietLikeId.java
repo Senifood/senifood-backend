fac962e2f955c0736618e782349b9a1d2ea5472a
oseoko <oseoko@naver.com>
2024-08-03 10:13:47 +0900

    feat: UserDietLikeId entity 생성

---
+++ src/main/java/likelion/senifood/entity/UserDietLikeId.java
@@ -0,0 +1,36 @@
+package likelion.senifood.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDietLikeId implements Serializable {
+    @Getter
+
+    private String userId;
+    private Integer dietId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDietLikeId that = (UserDietLikeId) o;
+        return Objects.equals(userId, that.userId) &&
+                Objects.equals(dietId, that.dietId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, dietId);
+    }
+}
